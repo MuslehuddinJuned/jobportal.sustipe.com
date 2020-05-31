4e68536f1f173ceab8609bb9225fc86e9cc5cd5c
Muslehuddin Juned <MuslehuddinJuned@users.noreply.github.com>
2020-06-01 01:40:22 +0600

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,7 +71,7 @@
         <div class="col-md-6">
             <h1 class="display-4 text-center">Search Your</h1>
             <h1 class="display-2 text-center">Dream</h1>
-            <h1 class="display-3 text-center">Job Here</h1>
+            <h1 class="display-3 text-center">Job Here . . . </h1>
 
         </div>
     </div>
